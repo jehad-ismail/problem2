5cc0f7a502c95c6928685ca49b6474c0340125b3
jehad-ismail <jehadanwar1@gmail.com>
2025-03-07 12:52:22 +0200

    Add files via upload

---
+++ Source2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2, sum = 0;
+	cout << " firt number =  ";
+	cin >> num1;
+	cout << "second number =   ";
+	cin >> num2;
+	
+	sum = num1 + num2;
+	cout << "sum =  " << sum << endl;
+
+
+
+
+
+	return 0;
+}
